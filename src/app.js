4761db211b0295b35dc145336c0a6def55ade0b2
toquinovic <j.toquica@udla.edu.co>
2023-10-20 21:53:06 -0500

    swager-test

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 
 //settings
@@ -11,6 +13,33 @@ app.set("port", process.env.BACK_PORT || 3000);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
+
+// Swagger definition
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Print Object Metaverse",
+      version: "0.1.0",
+      description: "This API is responsible for communicating a robot with metaverses",
+    },
+    servers: [
+      {
+        url: `http://localhost:${app.get("port")}`,
+      },
+    ],
+  },
+  // Path to the API docs
+  apis: ["./routes/*.js"],
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const specs = swaggerJsdoc(options);
+app.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 //routes
 app.use("/api", require("./routes"));
